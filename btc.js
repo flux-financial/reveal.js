6363b8355d1a33c6f26cd47ffc9ef039ff1dddaf
John Crossman <jgcrossman2@gmail.com>
2021-08-26 12:35:19 -0700

    add Bitcoin is Volatile slide

--- btc.js
+++ btc.js
@@ -6,8 +6,36 @@ http.get('https://api.coindesk.com/v1/bpi/historical/close.json ', function(err,
     if(err) {
         console.log(err);
     } else {
-        let dataload = JSON.parse(data);
-        console.log(dataload.bpi);
-
+        const dataload = JSON.parse(data);
+        const prices = dataload.bpi;
+        const results = Object.keys(prices).map((key) => [(key), prices[key]]);
+        let mean = 0;
+        const meanday = results.length;
+        let variance = 0;
+        let sd = 0;
+        const btctable = document.getElementById('btctable');
+        let tablefill = `<table>
+        <tr><th>Date</th><th>USD price</th><th>${meanday} Day Mean</th><th>1 SD</th></tr>`;
+        // calculate mean
+        for (let i=0; i<results.length; i++){
+            mean += results[i][1];
+        };
+        mean = mean / meanday;
+        // calculate 1 Standard Deviation
+        for (let i=0; i<results.length; i++){
+            variance += Math.pow(results[i][1] - mean, 2);
+        };
+        variance = variance / meanday;
+        sd = Math.sqrt(variance);
+        let x = meanday - 1;
+        tablefill += `<tr><td>${results[x][0]}</td><td>${results[x][1]}</td><td>${mean.toFixed(2)}</td><td>${sd.toFixed(2)}</td></tr>`;
+        tablefill += `</table>`;
+        btctable.innerHTML = tablefill;
+        /*
+        console.log(results[30][0]);
+        console.log(mean);
+        console.log(variance);
+        console.log(sd);
+        */
     }
 });
